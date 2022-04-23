130430ac2fb8b161eede3c8014dd55b36ba80cda
Simón Moreno <saimonm0504@gmail.com>
2022-04-22 23:44:15 -0400

    Update HashRouter in App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
 import { HashRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./Components/Home/Home";
+import Pokedex from "./Components/Pokedex_Section/Pokedex";
+import Error404 from "./Components/Ui/Error/Error404";
+import NoContent from "./Components/Ui/Error/NoContent";
 
 function App() {
     return (
         <HashRouter>
             <Routes>
                 <Route path='/' element={<Home />} />
-                {/* <Route path="Pokedex" element={<Pokedex />} />
-        <Route path="Pokemon">
-          <Route path=":name" element={<PokemonDetailsContainer />} />
-        </Route>
-        <Route path="Moves" element={<NoContent />} />
-        <Route path="Abilities" element={<NoContent />} />
-        <Route path="Berries" element={<NoContent />} />
-        <Route path="Location" element={<NoContent />} />
-        <Route path="Type_Charts" element={<NoContent />} />
-        <Route path="*" element={<Error404 />} /> */}
+                <Route path='Pokedex' element={<Pokedex />} />
+                {/* <Route path='Pokemon'>
+                    <Route path=':name' element={<PokemonDetailsContainer />} />
+                </Route> */}
+                <Route path='Moves' element={<NoContent />} />
+                <Route path='Abilities' element={<NoContent />} />
+                <Route path='Berries' element={<NoContent />} />
+                <Route path='Location' element={<NoContent />} />
+                <Route path='Type_Charts' element={<NoContent />} />
+                <Route path='*' element={<Error404 />} />
             </Routes>
         </HashRouter>
     );
