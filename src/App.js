dc76fffadb0c34bb634340d67e3878a0a1ac2f38
Simón Moreno <saimonm0504@gmail.com>
2022-04-22 23:18:22 -0400

    Set HashRouter and Routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
+import { HashRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import Home from "./Components/Home/Home";
 
 function App() {
     return (
-        <div className='App'>
-            <header className='text-4xl text-pink-500'>Culo</header>
-        </div>
+        <HashRouter>
+            <Routes>
+                <Route path='/' element={<Home />} />
+                {/* <Route path="Pokedex" element={<Pokedex />} />
+        <Route path="Pokemon">
+          <Route path=":name" element={<PokemonDetailsContainer />} />
+        </Route>
+        <Route path="Moves" element={<NoContent />} />
+        <Route path="Abilities" element={<NoContent />} />
+        <Route path="Berries" element={<NoContent />} />
+        <Route path="Location" element={<NoContent />} />
+        <Route path="Type_Charts" element={<NoContent />} />
+        <Route path="*" element={<Error404 />} /> */}
+            </Routes>
+        </HashRouter>
     );
 }
 
