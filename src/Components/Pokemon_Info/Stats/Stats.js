77cc4a41ee633b501d7d58f06123ba0004e14615
Simón Moreno <saimonm0504@gmail.com>
2022-04-23 00:12:14 -0400

    Fixed Stats file

--- src/Components/Pokemon_Info/Stats/Stats.js
+++ src/Components/Pokemon_Info/Stats/Stats.js
@@ -1,42 +1,15 @@
-import { useContext } from "react";
-import PokedexContext from "../../../Context/GlobalContext";
-// import PokedexContext from "../../../../../Context/GlobalContext";
+// import TypeDefense from "./Pokemon_Defense/TypeDefense";
+import BarStats from "./Pokemon_Stats/BarStats";
 
-const Training = () => {
-    const { singlePokemon, pokemonAbout } = useContext(PokedexContext);
-
+const Stats = ({ stats, color, types }) => {
     return (
-        <section className='mt-3 text-base'>
-            <p className='font-bold mb-3'>Training</p>
-            <div className='flex items-center my-2'>
-                <p
-                    className='text-gray-400 font-medium'
-                    title='The base experience gained for defeating this Pokémon.'
-                >
-                    Base Experience
-                </p>
-                <span className='pl-5'>
-                    {singlePokemon.base_experience} EXP
-                </span>
-            </div>
-            <div className='flex items-center my-2'>
-                <p className='text-gray-400 font-medium'>Base Happiness</p>
-                <span className='pl-5'>
-                    {((pokemonAbout.base_happiness / 255) * 100).toFixed(1)} %
-                </span>
-            </div>
-            <div className='flex items-center my-2'>
-                <p className='text-gray-400 font-medium'>Catch Rate</p>
-                <span className='pl-5'>
-                    {((pokemonAbout.capture_rate / 255) * 100).toFixed(1)} %
-                </span>
-            </div>
-            <div className='flex items-center my-2'>
-                <p className='text-gray-400 font-medium'>Growth Rate</p>
-                <span className='pl-5'>{pokemonAbout.growth_rate.name}</span>
-            </div>
+        <section
+            className={`${stats ? "statsSection active" : "statsSection"}`}
+        >
+            <BarStats color={color} />
+            {/* <TypeDefense types={types} /> */}
         </section>
     );
 };
 
-export default Training;
+export default Stats;
